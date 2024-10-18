c288c5e37d0082c50461d72b173b27ae1a9d185c
drcxd <drcxd@sina.com>
2024-10-18 11:48:50 +0800

    SICP exercise 3.79 and fix exercise 3.78 problem

--- solve-2nd.rkt
+++ solve-2nd.rkt
@@ -8,8 +8,16 @@
 ;; See comments in solve.rkt for the extra delay and force
 (define (solve-2nd a b dt y0 dy0)
   (define y (integral-delayed (delay (force dy)) y0 dt))
-  (define dy (integral-delayed (delay (force ddy)) dy0 dt))
+  (define dy (delay (integral-delayed (delay (force ddy)) dy0 dt)))
   (define ddy (delay (add-streams (scale-stream y b)
-                                  (scale-stream dy a))))
+                                  (scale-stream (force dy) a))))
   y)
 (#%provide solve-2nd)
+
+;; Exercise 3.79
+(define (solve-2nd-general f dt y0 dy0)
+  (define y (integral-delayed (delay (force dy)) y0 dt))
+  (define dy (delay (integral-delayed (delay (force ddy)) dy0 dt)))
+  (define ddy (delay (stream-map f y (force dy))))
+  y)
+(#%provide solve-2nd-general)
