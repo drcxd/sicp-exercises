8de596f1d7a20200edc22bfb6a2fe97629dbf264
drcxd <drcxd@sina.com>
2024-10-31 19:38:46 +0800

    Merge exercise 4.8 into the let package

--- evaluator-definition.rkt
+++ evaluator-definition.rkt
@@ -6,20 +6,29 @@
 (#%require "./evaluator-lambda.rkt")
 
 (define (definition? exp) (tagged-list? exp 'define))
+
 (define (definition-variable exp)
   (if (symbol? (cadr exp))
       (cadr exp)
       (caadr exp)))
+
 (define (definition-value exp)
   (if (symbol? (cadr exp))
       (caddr exp)
       (make-lambda (cdadr exp) ; formal parameters
                    (cddr exp)))) ; body
+
+(define (make-definition var value)
+  (list 'define var value))
+
 (define (eval-definition exp env)
   (define-variable! (definition-variable exp)
     (eval (definition-value exp) env)
     env)
   'ok)
+
 (define (install-definition!)
   (install-new-exp! 'define definition? eval-definition))
-(#%provide install-definition!)
+
+(#%provide install-definition!
+           make-definition)
