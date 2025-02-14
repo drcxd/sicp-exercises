cda89d69ada39ac2cb0146116895a5efb14cec0c
drcxd <drcxd@sina.com>
2025-02-14 10:21:42 +0800

    Implementation of explicit control evaluator given by text

---
+++ metacircular-evaluator-env.rkt
@@ -0,0 +1,157 @@
+#lang sicp
+
+(#%require "./filter.rkt")
+
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)
+        (list '+ +)
+        (list '- -)
+        (list '* *)
+        (list '/ /)))
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc))) primitive-procedures))
+
+;; -- begin environment
+
+;; Exercise 4.11
+
+;; The frame is implemented as a list of variable, value pairs
+
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+
+(define (make-frame variables values)
+  (cons 'frame (map cons variables values)))
+
+(define (frame-variables frame) (map car frame))
+(define (frame-values frame) (map cdr frame))
+(define (add-binding-to-frame! var val frame)
+  (set-cdr! frame (cons (cons var val) (cdr frame))))
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan var-list)
+      (cond ((null? var-list)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (caar var-list)) (cdar var-list))
+            (else (scan (cdr var-list)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+          (scan (cdr frame)))))
+  ;; Exercise 4.16 a
+  (let ((value (env-loop env)))
+    (if (eq? value '*unassigned*)
+        (error "Unassigned variable" var)
+        value)))
+
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan var-list)
+      (cond ((null? var-list)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (caar var-list)) (set-cdr! (car var-list) val))
+            (else (scan (cdr var-list)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable: SET!" var)
+        (let ((frame (first-frame env)))
+          (scan (cdr frame)))))
+  (env-loop env))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan var-list)
+      (cond ((null? var-list)
+             (add-binding-to-frame! var val frame))
+            ((eq? var (caar var-list)) (set-cdr! (car var-list) val))
+            (else (scan (cdr var-list)))))
+    (scan (cdr frame))))
+
+;; Exercise 4.12
+
+;; Alternative implementation
+
+;; (define (lookup-variable var env found not-found frame-end)
+;;   (define (env-iter env)
+;;     (if (null? env)
+;;         (not-found)
+;;         (let ((frame (first-frame env)))
+;;           (define (frame-iter frame)
+;;             (cond ((null? frame) (frame-end))
+;;                   ((eq? var (caar frame)) (found frame))
+;;                   (else (frame-iter (cdr frame)))))
+;;           (frame-iter frame))))
+;;   (env-iter env))
+
+;; (define (lookup-variable-value var env)
+;;   (lookup-variable
+;;    var
+;;    env
+;;    (lambda (frame) (cdar frame))
+;;    (lambda () (error "Unbound variable" var))
+;;    (lambda () (lookup-variable-value var (enclosing-environment env)))))
+
+;; (define (set-variable-value! var val env)
+;;   (lookup-variable
+;;    var
+;;    env
+;;    (lambda (frame) (set-cdr! (car frame) val))
+;;    (lambda () (error "Unbound variable: SET!" var))
+;;    (lambda () (set-variable-value! var val (enclosing-environment env)))))
+
+;; (define (define-variable! var val env)
+;;   (lookup-variable
+;;    var
+;;    env
+;;    (lambda (vals) (set-car! vals val))
+;;    (lambda () 'nothing)
+;;    (lambda () (add-binding-to-frame! var val (first-frame env)))))
+
+
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define the-global-environment (setup-environment))
+
+;; Exercise 4.13
+
+;; Only remove the binding from the first frame, because other code
+;; may still use the binding in the enclosing frame.
+
+;; The following procedure is based on the implementation of
+;; environment in exercise 4.11
+
+(define (remove-from-env! var env)
+  (let ((frame (first-frame env)))
+    (set-car! env (filter (lambda (binding) (not (eq? var (car binding)))) frame))))
+
+;; -- end environment
+
+(#%provide
+ extend-environment
+ lookup-variable-value
+ set-variable-value!
+ define-variable!
+ remove-from-env!
+ the-global-environment)
---
+++ metacircular-evaluator-syntax.rkt
@@ -0,0 +1,126 @@
+#lang sicp
+
+(#%require "./tagged-list.rkt")
+
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+(define (variable? exp) (symbol? exp))
+
+(define (quoted? exp) (tagged-list? exp 'quote))
+(define (text-of-quotation exp env) (cadr exp))
+
+(define (assignment? exp) (tagged-list? exp 'set!))
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+(define (definition? exp) (tagged-list? exp 'define))
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp) ; formal parameters
+                   (cddr exp)))) ; body
+
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (lambda? exp) (tagged-list? exp 'lambda))
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+(define (begin? exp) (tagged-list? exp 'begin))
+(define (begin-actions exp) (cdr exp))
+
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (no-operands? ops) (null? ops))
+(define (empty-arglist) '())
+(define (last-operands? ops) (null? (cdr ops)))
+(define (adjoin-arg arg arglist) (append arglist (list arg)))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+(define (procedure-parameters p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-environment p) (cadddr p))
+
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+(define (apply-primitive-procedure proc args)
+  (apply proc args))
+
+(define (true? x) (not (eq? false x)))
+
+(#%provide
+ self-evaluating?
+ variable?
+
+ quoted?
+ text-of-quotation
+
+ assignment?
+ assignment-variable
+ assignment-value
+
+ definition?
+ definition-variable
+ definition-value
+
+ if?
+ if-predicate
+ if-consequent
+ if-alternative
+
+ lambda?
+ lambda-parameters
+ lambda-body
+
+ begin?
+ begin-actions
+
+ application?
+ operator
+ operands
+ no-operands?
+ first-operand
+ rest-operands
+ last-operands?
+ empty-arglist
+ adjoin-arg
+
+ primitive-procedure?
+ compound-procedure?
+ make-procedure
+ procedure-parameters
+ procedure-body
+ procedure-environment
+
+ first-exp
+ last-exp?
+ rest-exps
+
+ apply-primitive-procedure
+
+ true?)
