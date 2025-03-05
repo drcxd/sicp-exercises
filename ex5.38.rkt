5444acc6023fffbeee47f38077994f6a14e9cd94
drcxd <drcxd@sina.com>
2025-03-05 15:10:29 +0800

    Exercise 5.38 d part draft

--- ex5.38.rkt
+++ ex5.38.rkt
@@ -4,27 +4,37 @@
 (#%require "./register-machine-simulator.rkt")
 (#%require "./register-machine-operations.rkt")
 
-(define controller
-  (statements
-   (compile '(begin
-               (define (factorial n)
-                 (define (iter product counter)
-                   (if (> counter n)
-                       product
-                       (iter (* counter product)
-                             (+ counter 1))))
-                 (iter 1 1))
-               (factorial 5)) 'val 'next)))
+;; (define controller
+;;   (statements
+;;    (compile '(begin
+;;                (define (factorial n)
+;;                  (define (iter product counter)
+;;                    (if (> counter n)
+;;                        product
+;;                        (iter (* counter product)
+;;                              (+ counter 1))))
+;;                  (iter 1 1))
+;;                (factorial 5)) 'val 'next)))
 
-controller
+;; controller
 
 (define initializer
   '((perform (op initialize-stack))
     (assign env (op get-global-environment))))
 
-(define fib-machine
-  (make-machine all-regs machine-operations
-                (append initializer controller)))
+;; (define fib-machine
+;;   (make-machine all-regs machine-operations
+;;                 (append initializer controller)))
 
-(start fib-machine)
-(get-register-contents fib-machine 'val)
+;; (start fib-machine)
+;; (get-register-contents fib-machine 'val)
+
+(define controller
+  (append initializer
+          (statements (compile '(* 1 2 3 4 5) 'val 'next))))
+
+controller
+
+(define test-machine (make-machine all-regs machine-operations controller))
+(start test-machine)
+(get-register-contents test-machine 'val)
